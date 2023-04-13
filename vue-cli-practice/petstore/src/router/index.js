28b54aafe6f4f88e20242cea1e669c3795a30868
kimsolee95 <ahhasolee@naver.com>
2023-04-13 22:30:41 +0900

    feat: [ROUTER-ADD] vue.js 2 내용 실습 (레이아웃 나누기)

--- vue-cli-practice/petstore/src/router/index.js
+++ vue-cli-practice/petstore/src/router/index.js
@@ -1,23 +1,44 @@
+// import { MAX_SAFE_INTEGER } from 'core-js/core/number'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+
+import Form from '../components/Form.vue'
+import Main from '../components/Main.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // },
+
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'iMain',
+    component: Main,
+    props: true
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/form',
+    name: 'Form',
+    component: Form,
+    props: true
+  },
+  // {
+  //   path: '*',
+  //   redirect: "/"
+  // }
 ]
 
 const router = new VueRouter({
