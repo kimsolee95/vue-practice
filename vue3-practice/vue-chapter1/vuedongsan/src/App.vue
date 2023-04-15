531f58faaff14b96102c7525360bfe3a536187bb
kimsolee95 <ahhasolee@naver.com>
2023-04-15 22:21:12 +0900

    feat: vue 3를 이용한 기본 데이터 바인딩 확인

---
+++ vue3-practice/vue-chapter1/vuedongsan/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div v-for="(data, idx) in products" :key="idx">
+    <h4>{{ data }}</h4>
+    <p> 50만원</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
