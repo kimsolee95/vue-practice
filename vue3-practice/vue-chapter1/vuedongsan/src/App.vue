770b6a46b943384d3f7d961f183c23432b9c8d3c
kimsolee95 <ahhasolee@naver.com>
2023-04-17 23:19:42 +0900

    feat: vue 3를 이용한 기본 데이터 바인딩 및 이벤트핸들러를 통한 click 감지 확인

--- vue3-practice/vue-chapter1/vuedongsan/src/App.vue
+++ vue3-practice/vue-chapter1/vuedongsan/src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <div v-for="(data, idx) in products" :key="idx">
+
+  <!-- <div v-for="(data, idx) in products" :key="idx">
     <h4>{{ data }}</h4>
     <p> 50만원</p>
+  </div> -->
+
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p> 50만원</p>
+    <button @click="increaseFirst">허위매물신고</button> <span>신고 수: {{ first }}</span>
   </div>
+
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p> 60만원</p>
+    <button @click="increaseSecond">허위매물신고</button> <span>신고 수: {{ second }}</span>
+  </div>
+
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p> 70만원</p>
+    <button @click="increaseThird">허위매물신고</button> <span>신고 수: {{ third }}</span>
+  </div> 
+
 </template>
 
 <script>
@@ -14,8 +34,24 @@ export default {
   data() {
     return {
 
+      first : 0,
+      second: 0,
+      third: 0,
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
+  },
+  methods: {
+
+    increaseFirst() {
+      this.first += 1;
+    },
+    increaseSecond() {
+      this.second += 1;
+    },
+    increaseThird() {
+      this.third += 1;
+    },
+
   },
   components: {
 
