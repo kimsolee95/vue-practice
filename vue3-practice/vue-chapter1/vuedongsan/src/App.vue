d6a899b219433b38a27fa7955c56b66626ad2bb9
kimsolee95 <ahhasolee@naver.com>
2023-04-22 09:46:22 +0900

    feat: vue modal ul 만들기

--- vue3-practice/vue-chapter1/vuedongsan/src/App.vue
+++ vue3-practice/vue-chapter1/vuedongsan/src/App.vue
@@ -1,24 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
 
+  <div class="black-bg" v-if="modalActivate == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="modalClose">닫기</button>
+    </div>
+  </div>
+
   <!-- <div v-for="(data, idx) in products" :key="idx">
     <h4>{{ data }}</h4>
     <p> 50만원</p>
   </div> -->
 
   <div>
-    <h4>{{ products[0] }}</h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="modalActivate = true">{{ products[0] }}</h4>
     <p> 50만원</p>
     <button @click="increaseFirst">허위매물신고</button> <span>신고 수: {{ first }}</span>
   </div>
 
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4>{{ products[1] }}</h4>
     <p> 60만원</p>
     <button @click="increaseSecond">허위매물신고</button> <span>신고 수: {{ second }}</span>
   </div>
 
   <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4>{{ products[2] }}</h4>
     <p> 70만원</p>
     <button @click="increaseThird">허위매물신고</button> <span>신고 수: {{ third }}</span>
@@ -33,6 +44,8 @@ export default {
   name: 'App',
   data() {
     return {
+
+      modalActivate: false,
 
       first : 0,
       second: 0,
@@ -51,6 +64,12 @@ export default {
     increaseThird() {
       this.third += 1;
     },
+    mocalOpen() {
+      this.modalActivate = true;
+    },
+    modalClose() {
+      this.modalActivate = false;
+    }
 
   },
   components: {
@@ -67,5 +86,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
