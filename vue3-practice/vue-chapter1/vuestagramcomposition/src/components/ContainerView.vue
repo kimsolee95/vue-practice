546371d9ef677ac9bf1da3c82a32eefc4bd19177
kimsolee95 <ahhasolee@naver.com>
2023-05-11 23:28:03 +0900

    feat: data binding 연습

---
+++ vue3-practice/vue-chapter1/vuestagramcomposition/src/components/ContainerView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <PostView v-for="item in postdata" 
+    :key="item"
+    :post="item"
+    />
+  </div>
+</template>
+<script>
+import PostView from './PostView.vue';
+
+export default{
+
+  components: {
+    PostView: PostView
+  },
+  props: {
+    postdata: {
+      type: Array,
+      required: true
+    }
+  }
+
+}
+</script>
+<style>
+</style>
