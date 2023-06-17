ec771d6604de226466da9be2aacf452cac63ffdc
kimsolee95 <ahhasolee@naver.com>
2023-06-17 23:01:46 +0900

    feat: vue3 practice project init, router install

---
+++ vue3-practice/vue-chapter1/vue3-posts/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>About Vue</h2>
+    <button @click="$router.push('/')" class="btn btn-primary">Home으로 이동</button>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss"></style>
---
+++ vue3-practice/vue-chapter1/vue3-posts/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Home View</h2>
+    <button @click="goAboutPage" class="btn btn-primary">About 이동</button>
+  </div>
+</template>
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+console.log('route.path: ', route.path)
+
+const goAboutPage = () => {
+  router.push('/about')
+}
+</script>
+<style lang="scss"></style>
