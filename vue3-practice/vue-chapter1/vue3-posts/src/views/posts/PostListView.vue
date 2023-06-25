32c5cb3efae21d8f859b7b83a6590a323b7a589b
kimsolee95 <ahhasolee@naver.com>
2023-06-25 12:39:54 +0900

    feat pagination 단위 설정 select box, 검색 필터 추가

--- vue3-practice/vue-chapter1/vue3-posts/src/views/posts/PostListView.vue
+++ vue3-practice/vue-chapter1/vue3-posts/src/views/posts/PostListView.vue
@@ -2,6 +2,22 @@
   <div>
     <h2>게시글 목록</h2>
     <hr class="my-4" />
+    <!-- search filter -->
+    <form @submit.prevent>
+      <div class="row g-3">
+        <div class="col">
+          <input v-model="params.title_like" type="text" class="form-control" />
+        </div>
+        <div class="col-3">
+          <select v-model="params._limit" class="form-select">
+            <option value="3">3개씩 보기</option>
+            <option value="6">6개씩 보기</option>
+            <option value="9">9개씩 보기</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <hr class="my-4" />
   </div>
 
   <div class="row">
@@ -16,6 +32,7 @@
     </div>
   </div>
 
+  <!-- pagination -->
   <nav class="mt-5" aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
       <li :class="{ disabled: !(params._page > 1) }" class="page-item">
@@ -56,7 +73,8 @@ const params = ref({
   _sort: 'createdAt',
   _order: 'desc',
   _page: 1,
-  _limit: 3
+  _limit: 3,
+  title_like: ''
 })
 //pagination
 const totalCount = ref(0)
