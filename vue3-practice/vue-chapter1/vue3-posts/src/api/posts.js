2309da89bf05987f73db159e2038af4165a876b1
kimsolee95 <ahhasolee@naver.com>
2023-06-25 11:03:04 +0900

    feat: pagination 적용

--- vue3-practice/vue-chapter1/vue3-posts/src/api/posts.js
+++ vue3-practice/vue-chapter1/vue3-posts/src/api/posts.js
@@ -8,8 +8,8 @@ const posts = [
   { id: 5, title: '제목5', content: '내용5', createdAt: '2020-05-01' }
 ]
 
-export function getPosts() {
-  return axios.get('http://localhost:5000/posts')
+export function getPosts(params) {
+  return axios.get('http://localhost:5000/posts', { params })
 }
 
 export function getPostById(id) {
--- vue3-practice/vue-chapter1/vue3-posts/src/views/posts/PostListView.vue
+++ vue3-practice/vue-chapter1/vue3-posts/src/views/posts/PostListView.vue
@@ -15,7 +15,27 @@
       </PostItem>
     </div>
   </div>
-  <hr class="my-4" />
+
+  <nav class="mt-5" aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <li :class="{ disabled: !(params._page > 1) }" class="page-item">
+        <a @click.prevent="--params._page" class="page-link" href="#">Previous</a>
+      </li>
+      <li
+        v-for="page in pageCount"
+        :key="page"
+        :class="{ active: params._page === page }"
+        class="page-item"
+      >
+        <a class="page-link" href="#" @click.prevent="params._page = page">{{ page }}</a>
+      </li>
+      <li :class="{ disabled: !(params._page < pageCount) }" class="page-item">
+        <a @click.prevent="++params._page" class="page-link" href="#">Next</a>
+      </li>
+    </ul>
+  </nav>
+
+  <hr class="my-5" />
   <AppCard>
     <PostDetailView :id="1" />
   </AppCard>
@@ -25,23 +45,41 @@ import PostItem from '@/components/posts/PostItem.vue'
 import PostDetailView from '@/views/posts/PostDetailView.vue'
 import AppCard from '@/components/AppCard.vue'
 
-import { ref, onMounted } from 'vue'
+import { ref, computed, watch, onMounted } from 'vue'
 import { getPosts } from '@/api/posts'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
 const posts = ref([])
+const params = ref({
+  _sort: 'createdAt',
+  _order: 'desc',
+  _page: 1,
+  _limit: 3
+})
+//pagination
+const totalCount = ref(0)
+const pageCount = computed(() => Math.ceil(totalCount.value / params.value._limit))
 
 onMounted(() => {
   fetchPosts()
 })
 
 const fetchPosts = async () => {
-  const { data } = await getPosts()
-  posts.value = data
+  try {
+    const { data, headers } = await getPosts(params.value)
+    posts.value = data
+    totalCount.value = headers['x-total-count']
+  } catch (error) {
+    console.log(error)
+  }
   // ({ data: posts.value } = await getPosts());
 }
+
+watch(params.value, () => {
+  fetchPosts()
+})
 
 const goPage = (postId) => {
   //router.push(`/posts/${postId}`)
