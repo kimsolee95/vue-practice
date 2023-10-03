7b4e3ce0f8fc273309ee296485858533409b80fd
kimsolee95 <ahhasolee@naver.com>
2023-10-03 19:07:09 +0900

    feat: plugin 사용 예시코드

--- vue3-practice/vue-chapter1/vue3-posts/src/main.js
+++ vue3-practice/vue-chapter1/vue3-posts/src/main.js
@@ -3,13 +3,18 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import funcPlugins from './plugins/func'
+import objPlugins from './plugins/obj'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(funcPlugins)
+app.use(objPlugins, { name: 'object plugin' })
+app.mount('#app')
 import 'bootstrap/dist/js/bootstrap.js'
 
-console.log('MODE:', import.meta.env.MODE)
-console.log('BASE_URL:', import.meta.env.BASE_URL)
-console.log('PROD:', import.meta.env.PROD)
-console.log('DEV:', import.meta.env.DEV)
-
-console.log('VITE_APP_API_URL:', import.meta.env.VITE_APP_API_URL)
+// console.log('MODE:', import.meta.env.MODE)
+// console.log('BASE_URL:', import.meta.env.BASE_URL)
+// console.log('PROD:', import.meta.env.PROD)
+// console.log('DEV:', import.meta.env.DEV)
+// console.log('VITE_APP_API_URL:', import.meta.env.VITE_APP_API_URL)
